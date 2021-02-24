8575268322db5cc9f6febe0da1686382ec5b0ef0
Miranda Montroy <Miranda_Montroy1@baylor.edu>
2021-02-24 13:11:56 -0600

    finished 3 and 4

--- sitka_4.py
+++ sitka_4.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import datetime
 
-open_file= open("sitka_weather_2018_simple.csv", "r")
+open_file= open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=',')
 
@@ -11,6 +11,7 @@ header_row= next(csv_file)
 
 for index, column_header in enumerate(header_row):
     print("Index:", index, "Column Name:", column_header)
+
 
 highs = []
 dates = []
@@ -24,11 +25,18 @@ lows = []
 
 
 for row in csv_file:
-    highs.append(int(row[5]))
-    lows.append(int(row[6]))
-    converted_date = datetime.strptime(row[2],'%Y-%m-%d' )
-    dates.append(converted_date)
+    try:
+        high = (int(row[4]))
+        low = (int(row[5]))
+        converted_date = datetime.strptime(row[2],'%Y-%m-%d')
 
+    except:
+        print(f"Missing data for {converted_date}")
+    
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(converted_date)
 
 #print(highs)
 
@@ -52,11 +60,3 @@ plt.tick_params(axis="both", labelsize=12)
 plt.show()
 
 
-#subplot example
-
-fig2, a = plt.subplots(2)
-
-a[0].plot(dates, highs, c='red')
-a[1].plot(dates, lows, c='blue')
-
-plt.show()
