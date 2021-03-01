1930c8a1e2ce5a54ed955d69d448aada75010eea
Miranda Montroy <Miranda_Montroy1@baylor.edu>
2021-03-01 16:54:06 -0600

    copied from sitka 4 and adjusted for automation

--- sitka_5.py
+++ sitka_5.py
@@ -15,5 +15,46 @@ for x in header_row:
     if x == "TMAX":
         tmax=counter
     
+    if x == "DATE":
+        date=counter
+
     counter +=1 
 
+highs = []
+dates = []
+lows = []
+
+for row in csv_file:
+    try:
+        high = (int(row[tmax]))
+        low = (int(row[tmin]))
+        converted_date = datetime.strptime(row[date],'%Y-%m-%d')
+
+    except:
+        print(f"Missing data for {converted_date}")
+    
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(converted_date)
+
+#print(highs)
+
+import matplotlib.pyplot as plt
+
+fig= plt.figure()
+
+plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
+
+#makes it label them diagonally to avoid overlap 
+fig.autofmt_xdate()
+
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+plt.title("Daily high and low temperatures in 2018", fontsize=16)
+plt.xlabel("", fontsize=12)
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis="both", labelsize=12)
+
+plt.show()
